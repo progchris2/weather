602ead465d313ad1e3fe0b07e17bf11d60d665fe
Gilles Chris <progchris@live.fr>
2018-05-03 14:47:39 +0200

    include weather

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var session = require("express-session");
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
+
+var expressLayouts = require('express-ejs-layouts');
+
+
 var app = express();
+
+app.use(
+    session({
+        secret: 'a4f8071f-c873-4447-8ee2',
+        resave: false,
+        saveUninitialized: false,
+    })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -18,6 +33,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var dataWeather = [];
+var errors = [];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  dataWeather =[];
+  errors = [];
+  res.render('index', { data: dataWeather, errors: errors });
 });
+
+
+router.post('/search', (req, res) => {
+
+  if (req.body.search.length > 0 ) {
+      dataWeather = [...dataWeather, req.body.search];
+      res.render('index', { data: dataWeather, errors});
+  }
+  else {
+      errors = [...errors, 'Le champs n\'est pas renseigné'];
+      res.render('index', { errors, data: dataWeather });
+      errors = [];
+  }
+})
+
+router.get('/delete', (req, res) => {
+    dataWeather.splice(req.query.id, 1);
+    res.render('index', { data: dataWeather, errors });
+})
 
 module.exports = router;
